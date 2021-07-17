cebaf43c22460c8fee33345afdb090533dec438f
honeydrop01 <sweet.bebe67@gmail.com>
2021-07-17 08:11:33 -0400

    connect db

---
+++ routes/database.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+
+// const DATABASE_URL = process.env.DATABASE_URL;
+const DATABASE_URL = 'postgres://heba:poop@localhost:5433/mydata';
+const pool = new Pool({
+  connectionString: DATABASE_URL
+});
+
+// a generic query, that executes all queries you send to it
+function query(text, values = []) {
+  return new Promise((resolve, reject) => {
+    pool.query(text, values)
+      .then((res) => {
+        resolve(res);
+      }).catch((err) => {
+        reject(err);
+      });
+  });
+}
+
+module.exports = {
+  query
+};
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var database = require("./database");
+var formidable = require("formidable");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
