d185feaf69bd213d212644f9d4b74a44f75ffeb7
honeydrop01 <sweet.bebe67@gmail.com>
2021-08-29 05:17:15 -0400

    remove next function

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,9 @@ var database = require("./database");
 var formidable = require("formidable");
 
 var fs = require("fs");
-const path = require("path");
 
 /* GET home page. */
-router.get("/", async function (req, res, next) {
+router.get("/", async function (req, res) {
   query = `SELECT id, name, type, path FROM images`;
   try {
     var { rows } = await database.query(query);
@@ -17,10 +16,10 @@ router.get("/", async function (req, res, next) {
   }
   console.log(rows);
 
-  res.render("index", { title: "home", pics: rows });
+  res.render("index", { title: "explore", pics: rows });
 });
 
-router.get("/search", async function (req, res, next) {
+router.get("/search", async function (req, res) {
   searchPics = req.flash("val");
   if (searchPics.length != 0) {
     console.log(searchPics);
@@ -31,7 +30,7 @@ router.get("/search", async function (req, res, next) {
 });
 
 async function searching(type) {
-  myquery = `SELECT images.id as id, images.path as path, images.name as name, users.name  FROM images JOIN users ON images.artist = users.id WHERE
+  myquery = `SELECT images.id as id, images.path as path, images.name as name, users.name  FROM images JOIN users ON images.user_id = users.id WHERE
            (type LIKE '%${type}%' OR images.name LIKE '%${type}%' OR description LIKE '% ${type}%' OR
              users.name LIKE '%${type}%')`;
   rows = [];
@@ -44,17 +43,17 @@ async function searching(type) {
   return myPic;
 }
 
-router.post("/search", function (req, res, next) {
+router.post("/search", function (req, res) {
   console.log("post method search", req.body.searchValue);
   req.flash("val", req.body.searchValue);
   res.redirect("/search");
 });
 
-router.get("/signup", function (req, res, next) {
+router.get("/signup", function (req, res) {
   res.render("signup", { title: "signup" });
 });
 
-router.post("/signup", async function (req, res, next) {
+router.post("/signup", async function (req, res) {
   var name = req.body.user;
   var email = req.body.email;
   var password = req.body.pass;
@@ -75,23 +74,27 @@ router.post("/signup", async function (req, res, next) {
 
   res.redirect("/login");
 });
-router.get("/login", function (req, res, next) {
-  res.render("login", { title: "login" });
+router.get("/login", function (req, res) {
+  invalid = req.query.invalid;
+
+  if (invalid) {
+    var msg = "invalid email or password";
+  }
+  res.render("login", { title: "login", msg: msg });
 });
-router.post("/login", async function (req, res, next) {
-  check = req.body.check;
+router.post("/login", async function (req, res) {
+  const check = req.body.check;
   data = await logIn(req.body);
   req.session.user = data;
-  console.log("session", req.session.user.name);
 
   if (data) {
     if (check) {
       res.cookie("user", req.session.user);
     }
-    res.redirect("/profile");
   } else {
-    res.redirect("/signup");
+    res.redirect("/login?invalid=1");
   }
+  res.redirect("/profile");
 });
 async function logIn(body) {
   var email = body.email;
@@ -115,24 +118,28 @@ async function logIn(body) {
     return null;
   }
 }
-router.get("/profile", async function (req, res, next) {
+router.get("/profile", async function (req, res) {
   user = req.session.user;
   console.log("profile", user);
-  query = `SELECT id, name, type, path FROM images WHERE artist = ${user.id}`;
+  query = `SELECT id, name, type, path FROM images WHERE user_id = ${user.id}`;
   try {
     var { rows } = await database.query(query);
   } catch (error) {
     console.log(error);
   }
   console.log(rows);
-  res.render("profile", { title: "profile", user: user, pics: rows });
+  res.render("profile", {
+    title: "profile",
+    user: user,
+    pics: rows,
+  });
 });
 
-router.get("/image/:id", async function (req, res, next) {
+router.get("/image/:id", async function (req, res) {
   var id = req.params.id;
-  console.log("its the pic id", id);
-  pic = `SELECT images.id, images.name, type, path, users.name as artistName FROM images join users ON
-       users.id = images.artist WHERE images.id = '${id}'`;
+  console.log("its the pic params", req);
+  pic = `SELECT images.id, images.name, type, path, user_id, users.name as artistName, users.id as userid FROM images join users ON
+       users.id = images.user_id WHERE images.id = '${id}'`;
   rows = [];
   try {
     var { rows } = await database.query(pic);
@@ -140,11 +147,19 @@ router.get("/image/:id", async function (req, res, next) {
     console.log(error);
   }
   console.log(rows);
-
-  res.render("image", { title: "image", photo: rows });
+  if (req.session.user) {
+    userid = req.session.user.id;
+  } else {
+    userid = null;
+  }
+  res.render("image", {
+    title: "image",
+    photo: rows,
+    userid: userid,
+  });
 });
 
-router.get("/logout", function (req, res, next) {
+router.get("/logout", function (req, res) {
   req.session.destroy(function () {
     console.log("user logged out");
   });
@@ -170,14 +185,14 @@ router.post("/form", function (req, res, next) {
     oldPath = files.image.path;
     console.log(oldPath);
     console.log(req.session.user.id, "digital user");
-    // const file = fs.readFileSync(oldpath);
     newPath = "/home/heba/Desktop/myapp/artgal/public/images/pics/" + picName;
     fs.rename(oldPath, newPath, function (err) {
       if (err) throw err;
     });
     imgPath = "images/pics/" + picName;
+    console.log("new type selection", fields.type);
 
-    q = `INSERT INTO images (name, type, description, path, artist)
+    q = `INSERT INTO images (name, type, description, path, user_id)
         VALUES($1, $2, $3, $4, $5)`;
     try {
       await database.query(q, [
@@ -188,7 +203,7 @@ router.post("/form", function (req, res, next) {
         req.session.user.id,
       ]);
       info = `SELECT * FROM images WHERE name = '${fields.picName}' AND
-           artist = '${req.session.user.id}'`;
+           user_id = '${req.session.user.id}'`;
       rows = [];
       try {
         var { rows } = await database.query(info);
@@ -203,7 +218,7 @@ router.post("/form", function (req, res, next) {
     res.redirect("/image/" + picid);
   });
 });
-router.post("/delete/:id", async function (req, res, next) {
+router.post("/delete/:id", async function (req, res) {
   var id = req.params.id;
   console.log(id, "deleted pic id");
   remove = `DELETE FROM images WHERE id = ${id} `;
@@ -215,7 +230,7 @@ router.post("/delete/:id", async function (req, res, next) {
   res.redirect("/profile");
 });
 
-router.get("/edit/:id", async function (req, res, next) {
+router.get("/edit/:id", async function (req, res) {
   var id = req.params.id;
   query = `SELECT id, name, type, path, description FROM images WHERE id = '${id}'`;
   try {
@@ -228,10 +243,10 @@ router.get("/edit/:id", async function (req, res, next) {
   res.render("edit", { title: "edit", img: rows, id: id });
 });
 
-router.post("/edit/:id", async function (req, res, next) {
+router.post("/edit/:id", async function (req, res) {
   var id = req.params.id;
   console.log(id, "edit id");
-  form = new formidable.IncomingForm();
+  var form = new formidable.IncomingForm();
   form.parse(req, async function (err, fields, files) {
     var newName = fields.picName;
     var newType = fields.type;
