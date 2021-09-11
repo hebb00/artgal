f975ced3b7b47b89ff9d5067323e3541b7e6bd88
honeydrop01 <sweet.bebe67@gmail.com>
2021-09-11 17:27:48 -0400

    article crud

--- routes/art.js
+++ routes/art.js
@@ -31,18 +31,98 @@ async function check(req, res, next) {
   res.redirect("/login");
 }
 
-router.get("/", function (req, res, next) {
+router.get("/", async function (req, res, next) {
   if (req.session.user) {
     res.locals.user = req.session.user;
   } else {
     res.locals.user = null;
   }
-  res.render("articles", { title: "articles" });
+  var data = `SELECT * FROM articles `;
+  try {
+    var { rows } = await database.query(data);
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.render("articles", { title: "articles", articles: rows });
 });
 
 router.get("/form", check, function (req, res, next) {
   res.render("art-form", { title: "form" });
 });
-router.post("/form", async function (req, res, next) {});
+router.post("/form", check, async function (req, res, next) {
+  var form = new formidable.IncomingForm();
+  form.parse(req, async function (err, fields, files) {
+    var title = fields.title;
+    var article = fields.article;
+    var query = `INSERT INTO articles (title,article,user_id )
+          VALUES($1, $2, $3)`;
+    try {
+      await database.query(query, [title, article, res.locals.user.id]);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  res.redirect("/art/");
+});
+
+router.get("/details/:id", async function (req, res, next) {
+  var id = req.params.id;
+  var get = ` SELECT * FROM articles WHERE art_id = ${id}`;
+  try {
+    var { rows } = await database.query(get);
+  } catch (error) {
+    console.log(error);
+  }
+  console.log("details", rows);
+  if (req.session.user) {
+    var user = req.session.user;
+  } else {
+    user = null;
+  }
+  res.render("details", { title: "details", article: rows[0], user: user });
+});
+router.post("/article/:id", async function (req, res, next) {
+  var id = req.params.id;
+  var del = `DELETE FROM articles WHERE art_id = ${id}`;
+  try {
+    await database.query(del);
+  } catch (error) {
+    console.log(error);
+  }
+  console.log("deleted");
+  res.redirect("/art/");
+});
+
+router.post("/modify/:id", async function (req, res, next) {
+  var id = req.params.id;
+  var form = new formidable.IncomingForm();
+  form.parse(req, async function (err, fields, files) {
+    var title = fields.title;
+    var article = fields.article;
+    var update = `UPDATE articles SET title = '${title}',
+     article = '${article}' WHERE art_id = ${id} `;
+    try {
+      await database.query(update);
+    } catch (error) {
+      console.log(error);
+    }
+    console.log("deleted");
+    res.redirect("/art/");
+  });
+});
+
+router.get("/modify/:id", check, async function (req, res, next) {
+  var id = req.params.id;
+  query = `SELECT art_id, title, article FROM articles WHERE art_id = '${id}'`;
+  try {
+    var { rows } = await database.query(query);
+  } catch (error) {
+    console.log(error);
+  }
+
+  console.log("modify", rows);
+  res.render("modify", { title: "modify", article: rows[0] });
+});
 
 module.exports = router;
